720f6286070be6503ab72e8a3bb76349a1aafd8b
RenLishisan <renshisan@qq.com>
2020-07-02 20:14:10 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
-import Home from '@/views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
+
 
 const routes: Array<RouteConfig> = [
     {
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
     {
         path: '/statistics',
         component: Statistics
+    },
+    {
+        path: '*',
+        component: NotFound
     }
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>404</div>
+        <div>
+            <router-link to="/">点击回到首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'NotFound.vue'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
