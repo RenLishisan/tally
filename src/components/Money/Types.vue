4ea49a5616833b9466aa89f543a68a3a1092d96a
RenLishisan <renshisan@qq.com>
2020-07-08 15:07:56 +0800

    模块化： 把Money.vue拆分成4个子组件。

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <ul class="types">
+            <li class="selected">支出</li>
+            <li>收入</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Types.vue'
+    };
+</script>
+
+<style lang="scss" scoped>
+    .types {
+        background: #c4c4c4;
+        display: flex;
+        text-align: center;
+        font-size: 24px;
+
+        > li {
+            width: 50%;
+            height: 64px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+
+            &.selected::after {
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 100%;
+                height: 4px;
+                background: #333;
+            }
+        }
+    }
+
+</style>
