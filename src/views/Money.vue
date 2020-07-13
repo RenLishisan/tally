9a9b02cee6b26077026212e234801211839da56a
RenLishisan <renshisan@qq.com>
2020-07-13 15:12:35 +0800

    把record的复制保存至list

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
     <layout class-prefix="layout">
         {{record}}
-        <NumberPad :value="record.amount" @update:value="onUpdateAmount"/>
+        <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <Notes @update:value="onUpdateNotes"/>
         <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -27,6 +27,7 @@
         components: {Tags, Notes, Types, NumberPad},
     })
     export default class Money extends Vue {
+        recordList: Record[]=[]
         tags = ['衣', '食', '住', '行', '意外'];
         record: Record = {
             tags: [], notes: '', type: '-', amount: 0
@@ -34,10 +35,21 @@
 
         onUpdateTags(value: string[]) {
             this.record.tags = value;
+            this.recordList.push(this.record)
         }
 
         onUpdateNotes(value: string) {
             this.record.notes = value;
+            console.log(this.recordList)
+        }
+        saveRecord(){
+            const record2 = JSON.parse(JSON.stringify(this.record));
+            this.recordList.push(record2);
+            console.log(this.recordList)
+        }
+        @Watch('recordList')
+        onRecordListChange(){
+            window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
         }
     }
 </script>
