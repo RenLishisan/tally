2bdfe478e91886648126996477af0cebfad20ac0
RenLishisan <renshisan@qq.com>
2020-07-21 21:03:06 +0800

    将 store进行模块化重构 删除 models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,26 +1,8 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => recordListModel.create(record),
-    tagList: tagListModel.fetch(),
-    findTag(id: string) {
-        return this.tagList.filter(t => t.id === id)[0];
-    },
-    createTag: (name: string) => {
-        const message = tagListModel.create(name);
-        if (message === 'duplicated') {
-            window.alert('这个标签重复啦！');
-        } else if (message === 'success') {
-            window.alert('标签添加成功！');
-        }
-    },
-    removeTag: (id: string) => {
-        return tagListModel.remove(id);
-    },
-    updateTag: (id: string, name: string) => {
-        return tagListModel.update(id, name);
-    }
+    ...recordStore,
+    ...tagStore
 };
 export default store;
