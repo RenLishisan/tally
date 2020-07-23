280333a7925c56b0225e9905370cb5fdda44ffee
RenLishisan <renshisan@qq.com>
2020-07-23 10:12:17 +0800

    EditLabel.vue 使用Vuex重构

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,49 +2,53 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
-import tagStore from '@/store/tagStore';
 
 Vue.use(Vuex);
 
-const store =  new Vuex.Store({
+type RootState = {
+    recordList: RecordItem[];
+    tagList: Tag[];
+    currentTag?: Tag;
+}
+const store = new Vuex.Store({
     state: {
-        recordList:[] as RecordItem[],
-        tagList:[] as Tag[]
-    },
+        recordList: [],
+        tagList: [],
+        currentTag: undefined
+    } as RootState,
     mutations: {
+        setCurrentTag(state, id: string) {
+            state.currentTag = state.tagList.filter(t => t.id === id)[0];
+        },
         fetchRecords: function (state) {
             state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
         },
-        createRecord(state,record){
+        createRecord(state, record) {
             const record2: RecordItem = clone(record);
             record2.createdAt = new Date();
             state.recordList.push(record2);
-            store.commit('saveRecords')
+            store.commit('saveRecords');
         },
         saveRecords(state) {
             window.localStorage.setItem('recordList',
                 JSON.stringify(state.recordList));
         },
         fetchTags(state) {
-            const tagList = JSON.parse(window.localStorage
-                .getItem('tagList') || '[]');
-            return state.tagList = tagList
+            state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
         },
-        createTag(state,name: string) {
+        createTag(state, name: string) {
             const names = state.tagList.map(item => item.name);
             if (names.indexOf(name) >= 0) {
                 window.alert('这个标签重复啦！');
-                return 'duplicated';
             }
             const id = createId().toString();
             state.tagList.push({id: name, name: name});
-            store.commit('saveTags')
+            store.commit('saveTags');
             window.alert('标签添加成功！');
-            return 'success';
         },
         saveTags(state) {
             window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
-        }
+        },
     }
 });
 
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -26,10 +26,13 @@
         components: {Button, FormItem}
     })
     export default class EditLabel extends Vue {
-        tag?: Tag = undefined;
+        get tag() {
+            return this.$store.state.currentTag;
+        }
 
         created() {
-            // this.tag = {} store.findTag(this.$route.params.id);
+            const id = this.$route.params.id;
+            this.$store.commit('setCurrentTag', id);
             if (!this.tag) {
                 this.$router.replace('/404');
             }
@@ -43,7 +46,7 @@
 
         remove() {
             if (this.tag) {
-                return
+                return;
                 // if (store.removeTag(this.tag.id)) {
                 //     this.$router.back();
                 // } else {
