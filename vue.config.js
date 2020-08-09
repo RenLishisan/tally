a87028fdb81669e51960569f136869bf982ca8ad
RenLishisan <renshisan@qq.com>
2020-08-09 21:34:37 +0800

    将网站部署到 GitHub tally-website 仓库

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require("path")
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/tally-website/'
+        : '/',
     lintOnSave: false,
     chainWebpack: config => {
         const dir = path.resolve(__dirname, 'src/assets/icons')
